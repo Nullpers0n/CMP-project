0bf36d0f1a37e7a664fc1c3e5a6692a21871d59e
CruzerBladeHD <44523083+CruzerBladeHD@users.noreply.github.com>
2018-11-05 20:20:29 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,51 +7,102 @@ using namespace std;
 
 class Employee {
 private:
-	string ID, name;
+	// I changed it to eID (employee ID) and the project ID to pID so we don't get 
+	// confused when we use them
+	string eID, name;
 	//…add more attributes as needed
 public:
 	Employee();  //constructor that does something
 	Employee(string myname, string myid);   //sets myname and myid to "null" maybe or sets the name and id for a certain employee
 	string getID();  //prints/returns out the id number (not an int after all)
 	string getName(); //prints/returns out the name of the employee
-	//…add other functions as needed, maybe setters or destructors are needed
+					  //…add other functions as needed, maybe setters or destructors are needed
 };
 
 
+// ---------- class Emplyee functions ----------
+
+// default constrcutor 
+Employee::Employee()
+{
+	name = "null";
+	eID = "0";
+}
+
+// constrcutor 
+Employee::Employee(string myname, string myid)
+{
+	name = myname;
+	eID = myid;
+}
+
+// employee get functions
+string Employee::getID()
+{
+	return eID;
+}
+
+string Employee::getName()
+{
+	return name;
+}
+
+// ------------ class porject -------------
 class Project {
 private:
-	string ID, title;
+	string pID, title;
 	//…add more attributes as needed
 public:
 	Project();  //constructor that does something
 	Project(string mytitle, string myid);  //constructor that sets both strings to "null" or something else
 	string getID(); //prints/returns out the id
 	string getTitle(); //prints/returns out the title
-	//…add other functions as needed,  maybe setters or destructors are needed
+					   //…add other functions as needed,  maybe setters or destructors are needed
 };
 
-////-----------------------------------------
 
+////----------------------class project functions----------
+Project::Project()
+{
+	title = "null";
+	pID = "0";
+}
+
+Project::Project(string mytitle, string myid)
+{
+	title = mytitle;
+	pID = myid;
+}
+
+string Project::getID()
+{
+	return pID;
+}
+
+string Project::getTitle()
+{
+	return title;
+}
 class ProjectEmployeeAssignment {
 private:
 	string pID, eID;  //project id and employee id
 	bool completed;  //project completed or incompleted
-	//…add more attributes as needed
+					 //…add more attributes as needed
 public:
 	ProjectEmployeeAssignment();  //constructor that does something
-	ProjectEmployeeAssignment (string mypID, string myeID, bool complete = false); //constructor that does something
+	ProjectEmployeeAssignment(string mypID, string myeID, bool complete = false); //constructor that does something
 	string getpID();  //prints/returns out the project id
 	string geteID();  //prints/returns out the employee id
 	bool isCompleted();  //prints/returns out the completion
 	void setCompleted(bool flag) { completed = flag; };  //looks like it is already implemented, suspicious
-	//…add other functions as needed
+														 //…add other functions as needed
 };
 
 class EmployeeList {
 private:
 	vector<Employee> listofEmployees;  //this will be used to return to the employee txt file
 	string empFileName;  //there it is, it stores the txt file name
-	//…add more attributes as needed 
+						 //…add more attributes as needed 
 public:
 	EmployeeList(); //reads from the file, oh god no, keep calm
 	void addEmployee(Employee e);  //adds an employee to the vector above 
@@ -60,7 +111,7 @@ public:
 	int findEmployeeIndexInVector(string ID);  //returns the id of the employee from the vector above
 	void listEmployees();  //doesn't return anything but lists the employees
 	~EmployeeList(); //writes back to the same input file, wait what??? (oh, a destructor)
-		//…add other functions as needed
+					 //…add other functions as needed
 };
 
 ////-----------------------------------------
@@ -69,7 +120,7 @@ class ProjectList {
 private:
 	vector<Project> listofProjects;  //stores a list of projects to be returned to project txt file
 	string prjFileName;  //oh god here is the filename 
-	//…add more attributes as needed
+						 //…add more attributes as needed
 public:
 	ProjectList(); //a constructor that reads from the file
 	void addProject(Project p); //adds a project to the vector
@@ -80,14 +131,14 @@ public:
 	int findProjectIndexInVector(string ID);  //returns project id
 	void listProjects();   //prints out (does not return) the list of projects
 	~ProjectList(); //writes back to the same input file
-		//…add other functions as needed
+					//…add other functions as needed
 };
 
 class listofPrjEmpAssignment {
 private:
 	vector <ProjectEmployeeAssignment> list;  //stores the list of projects that are assigned to the employees
 	string prj_empFileName;  //opens the assignment txt file
-	//…add more attributes as needed
+							 //…add more attributes as needed
 public:
 	listofPrjEmpAssignment();  //constructor that reads from the file and maybe stores into the vector
 	void addEmpPrjAssignment(Project b, Employee e);  //takes the corresponding project and employee and adds them to the vector?
@@ -99,13 +150,14 @@ public:
 	void listAllCompleteProjects();   //lists all complete projects 
 	void listAllProjectsAssignments();  //lists everything
 	~listofPrjEmpAssignment();   //destructor that writes back to the file
-		//…add other functions as needed
+								 //…add other functions as needed
 };
 
 
 
-void main() {
-
-	cout << "Working from the ground, removed all comments so that what is needed can be seen" << endl;
-
+int main() 
+{
+	// to take the option of the menue 
+	char option;
+	return 0;
 }
