f4da64561427178000fd5e9dfa4eadf9b837da6f
Nullpers0n <44268160+Nullpers0n@users.noreply.github.com>
2018-11-11 19:45:03 +0400

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -12,11 +12,29 @@ private:
 	string ID, name;
 	//…add more attributes as needed
 public:
+	//Purpose: setting ID to 000 and name to NULL
+	//Input : nothing
+	// Output : none
 	Employee();
+	//Purpose: setting ID to myid and name to myname
+	// Input: string myname and string myid that were inputted by the user
+	// Output: nothing
 	Employee(string myname, string myid);
+	//Purpose : Gets the ID of the employee
+	// Input : None
+	// Output : id of the employee
 	string getID();
+	//Purpose : gets the name of the employee
+	// Input: none
+	// output : name of the employe
 	string getName();
+	//Purpose : sets id to myid 
+	// input: string myid that was inputted by the user
+	//output : none
 	void setID(string myid);
+	// Purpose : sets name to myname
+	// input : string myname that was inputted by the user
+	// output : none
 	void setName(string myname);
 
 	//…add other functions as 
