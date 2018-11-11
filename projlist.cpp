65407f2bd95e22d0364fa965ac3035b128194b9f
Nullpers0n <44268160+Nullpers0n@users.noreply.github.com>
2018-11-11 17:28:05 +0400

    Update projlist.cpp
    ProjectList.cpp but with comments above the functions

--- projlist.cpp
+++ projlist.cpp
@@ -1,18 +1,27 @@
 
 #include "ProjectList.h"
 using namespace std;
-// non member function used in some of the classes to print error. 
+
+// Purpose: non member function used in some of the classes to print error.
+// Input: string from another input or function
+// Output: no output
 void ProjectList::printError(string& str)
 {
 	cout << "Error: ID or name you entered doesn't exist, enter again: ";
 	cin >> str;
 }
 
+// Purpose: returns the number of projects inside the list
+// Input: no input
+// Output: the size of listofProjects vector
 int ProjectList::getPrjListSize()
 {
 	return listofProjects.size();
 }
 
+// Purpose: Searches if a project is in the list of projects
+// Input: Project class (ID and title inside it)
+// Output: returns the index where the project is located in the vector, else it returns -1 as an indicator that the project was not found
 int ProjectList::isItInList(Project p)
 {
 	for (int i = 0; i< listofProjects.size(); i++)
@@ -26,6 +35,9 @@ int ProjectList::isItInList(Project p)
 }
 
 // ------add project function
+// Purpose: Adds a new project to the list of projects vector
+// Input: A Project class (ID and title inside it)
+// Output: returns true if the project was added, returns false if the project already exists
 bool ProjectList::addProject(Project p)
 {
 	// if we assume that the Project's private components are empty, we need more statements 
@@ -47,6 +59,9 @@ bool ProjectList::addProject(Project p)
 }
 
 //------ GET index using ID function 
+// Purpose: searches for the project that has the same id as the user entered
+// Input: the id that the user is searching for
+// Output: returns the index that the project is located on inside the listofprojects vector, else it returns -1
 int ProjectList::getIndexUsingID(string myid)
 {
 	for (int i = 0; i < listofProjects.size(); i++)
@@ -60,6 +75,9 @@ int ProjectList::getIndexUsingID(string myid)
 }
 
 // --------- Get indtex using title functiong
+// Purpose: searches for the project that has the same title as the user entered
+// Input: the title that the user is searching for
+// Output: returns the index that the project is located on inside the listofprojects vector, else it returns -1
 int ProjectList::getIndexUsingTitle(string mytitle)
 {
 	for (int i = 0; i < listofProjects.size(); i++)
@@ -72,7 +90,9 @@ int ProjectList::getIndexUsingTitle(string mytitle)
 	return -1;
 }
 //=-------- constrcutor 
-
+// Purpose: Opens the text file that contains the projects and stores them into the listofprojects vector
+// Input: No Input
+// Output: no output
 ProjectList::ProjectList()
 {
 	// used to push back to the project vector 
@@ -97,7 +117,9 @@ ProjectList::ProjectList()
 }
 
 // -------delete project 
-
+// Purpose: Deletes a project out of the listofprojects
+// Input: the id of the project that the user entered
+// Output: no output
 void ProjectList::deleteProject(string myid)
 {
 
@@ -126,6 +148,9 @@ void ProjectList::deleteProject(string myid)
 }
 
 //--------get project function
+// Purpose: searches for the project that the user wants throught its index
+// Input: the index that the project lies inside the vector
+// Output: the Project P
 Project ProjectList::getProject(int index)
 { 
 	Project p;
@@ -139,6 +164,9 @@ Project ProjectList::getProject(int index)
 }
 
 //-------------- get project title
+//Purpose : gets the project title and displays it to the user through the id
+// Input: ID of the project
+// output: The title of the project
 string ProjectList::getProjectTitle(string ID)
 {
 	int index;
@@ -152,7 +180,9 @@ string ProjectList::getProjectTitle(string ID)
 }
 
 //-------GET PROJECT ID
-
+// Purpose: Searches for the title of the project that the user entered to show the user the project id
+// Input : the title of the project that the user entered
+// output : The ID of the project
 string ProjectList::getProjectID(string mytitle)
 {
 	int index = getIndexUsingTitle(mytitle);
@@ -163,6 +193,9 @@ string ProjectList::getProjectID(string mytitle)
 	}
 	return listofProjects[index].getID();
 }
+//Purpose : Lists all the projects inside the list
+// Input: no input
+// Output: the cout that shows the user the project id and title
 void ProjectList::listProjects()
 {
 	for (int i = 0; i < listofProjects.size(); i++)
@@ -170,7 +203,9 @@ void ProjectList::listProjects()
 		cout << "Project ID: " << listofProjects[i].getID() << " Project Title: " << listofProjects[i].getTitle() << endl;
 	}
 }
-
+// Purpose : Destructor that returns the listofprojects vector back to the text file
+// Input: no Input
+// Output: no output
 ProjectList::~ProjectList() {
 
 	ofstream mycout("Projectlist.txt");
